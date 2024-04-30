8a00eea5e017c60d0c7f8955fd0c52d47c2343cb
jingxuyy <jingxuyy@163.com>
2024-04-30 14:05:46 +0800

    xu

---
+++ sg-vue-admin/src/views/dashboard/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">
+      博客管理系统
+    </div>
+    <div>
+      落花有意随流水，流水无心恋落花。
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
